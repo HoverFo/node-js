aacd238c7b1b88b0a65e561cd89dd075e87328ba
HoverFo <hoverfox23@gmail.com>
2023-03-21 15:49:13 +0100

    Test

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var studentRouter = require('./routes/student');
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// Intégration de la bdd
+var connectionString = "mongodb+srv://root:root@iut.ag7plwz.mongodb.net/test";
+var mongoDB = process.env.MONGODB_URI || connectionString;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/student', studentRouter);
+
+module.exports = app;
