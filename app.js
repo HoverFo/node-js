fc69985937b0cb9334ce3c6ce2676895a5f65056
HoverFo <hoverfox23@gmail.com>
2023-03-27 14:10:38 +0200

    Cors

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 var studentRouter = require('./routes/student');
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+
 
 // Intégration de la bdd
 var connectionString = "mongodb+srv://root:root@iut.ag7plwz.mongodb.net/test";
@@ -17,11 +20,15 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/student', studentRouter);
